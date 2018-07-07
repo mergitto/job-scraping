d47191a23aa3131d0d16e4e2094d516e6ca46a02
mergitto <mfm17101@bene.fit.ac.jp>
2018-07-07 17:29:54 +0900

    メモリの問題を解決

--- pluck.py
+++ pluck.py
@@ -9,13 +9,13 @@ df = df[df['id'] != 'id']
 df = df['tweet']
 df = df.dropna()
 
-df = df.str.replace("\<.+?\>", "", regex=True)
-df = df.str.replace("\[.+?\]", "", regex=True)
-df = df.str.replace("\【.+?\】", "", regex=True)
-df = df.str.replace("https?://[\w/:%#\$&\?\(\)~\.=\+\-]+", "", regex=True)
-df = df.str.replace("[#＃][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー]+", "", regex=True)
-df = df.str.replace("[@＠][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー_]+", "", regex=True)
-#df = df.str.replace("[^ぁ-んァ-ンーa-zａ-ｚA-ZＡ-Ｚ0-9一-龠０-９\-\r]", "", regex=True)
+df = df.str.replace("\<.+?\>", "")
+df = df.str.replace("\[.+?\]", "")
+df = df.str.replace("\【.+?\】", "")
+df = df.str.replace("https?://[\w/:%#\$&\?\(\)~\.=\+\-]+", "")
+df = df.str.replace("[#＃][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー]+", "")
+df = df.str.replace("[@＠][Ａ-Ｚａ-ｚA-Za-z一-鿆0-9０-９ぁ-ヶｦ-ﾟー_]+", "")
+df = df.str.replace("[^ぁ-んァ-ンーa-zａ-ｚA-ZＡ-Ｚ0-9一-龠０-９\-\r]", "")
 
 df.to_csv(sys.argv[2], index=None)
 
